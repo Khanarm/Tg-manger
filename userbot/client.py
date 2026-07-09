7882753cdef89da4c4f9e1a546236be56fd1f7fb
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-09 22:10:20 +0530

    Update client.py

--- userbot/client.py
+++ userbot/client.py
@@ -1,6 +1,10 @@
+from telethon import TelegramClient
 from telethon.sessions import StringSession
+from telethon.tl.types import Channel
 
-TelegramClient(
+from config import API_ID, API_HASH, STRING_SESSION
+
+client = TelegramClient(
     StringSession(STRING_SESSION),
     API_ID,
     API_HASH
@@ -9,18 +13,15 @@ TelegramClient(
 async def start_userbot():
     await client.start()
 
-
 async def get_channels():
     channels = []
 
     async for dialog in client.iter_dialogs():
         if isinstance(dialog.entity, Channel):
             if dialog.entity.broadcast:
-                channels.append(
-                    {
-                        "id": dialog.entity.id,
-                        "title": dialog.name
-                    }
-                )
+                channels.append({
+                    "id": dialog.entity.id,
+                    "title": dialog.name
+                })
 
     return channels
