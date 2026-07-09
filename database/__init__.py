f3ddfbe45ac63bab08d3299992e2003d5ae72125
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-09 23:40:53 +0530

    Import additional functions from mongo module

--- database/__init__.py
+++ database/__init__.py
@@ -1,1 +1,10 @@
 from .mongo import *
+from .mongo import (
+    save_userbot,
+    get_userbot,
+    get_all_userbots,
+    save_channel,
+    get_channel,
+    get_all_channels,
+    delete_channel,
+)
