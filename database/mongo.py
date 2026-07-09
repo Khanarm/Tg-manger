9477f7a5dc635f4f4d3a09ba4b5a4bdca9a5506c
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-09 19:31:39 +0530

    Update mongo.py

--- database/mongo.py
+++ database/mongo.py
@@ -4,3 +4,9 @@ from config import MONGO_URI, DATABASE_NAME
 client = AsyncIOMotorClient(MONGO_URI)
 
 db = client[DATABASE_NAME]
+
+users = db.users
+channels = db.channels
+settings = db.settings
+userbots = db.userbots
+broadcasts = db.broadcasts
