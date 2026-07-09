e3975ed1e8fd8f440ef06cfa340516cb73413cf8
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-09 18:19:44 +0530

    Create bot.py to set up Telegram bot
    Initialize bot with routers and start polling.

---
+++ bot.py
@@ -0,0 +1,26 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+
+from config import BOT_TOKEN
+from handlers.start import router as start_router
+from handlers.setname import router as setname_router
+from handlers.broadcast import router as broadcast_router
+
+from userbot.client import start_userbot
+
+bot = Bot(BOT_TOKEN)
+dp = Dispatcher()
+
+dp.include_router(start_router)
+dp.include_router(setname_router)
+dp.include_router(broadcast_router)
+
+
+async def main():
+    await start_userbot()      # Userbot start
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
