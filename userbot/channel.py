e13b5b24f7a8f2c4d5b3d581cee8ce05604c0d02
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-09 22:50:17 +0530

    Update channel.py

--- userbot/channel.py
+++ userbot/channel.py
@@ -1,6 +1,9 @@
 from telethon.tl.types import Channel
 from userbot.client import get_all_clients
 
+from telethon import functions
+from telethon.tl.functions.channels import GetFullChannelRequest
+from telethon.tl.functions.messages import GetHistoryRequest
 
 async def get_channels():
     """
@@ -33,3 +36,48 @@ async def get_channels():
                 })
 
     return channels
+
+async def get_channel_info(channel_id: int):
+
+    clients = get_all_clients()
+
+    for userbot_id, client in clients.items():
+
+        try:
+            entity = await client.get_entity(channel_id)
+
+            full = await client(GetFullChannelRequest(entity))
+
+            subscribers = full.full_chat.participants_count or 0
+
+            history = await client(
+                GetHistoryRequest(
+                    peer=entity,
+                    limit=1,
+                    offset_date=None,
+                    offset_id=0,
+                    max_id=0,
+                    min_id=0,
+                    add_offset=0,
+                    hash=0
+                )
+            )
+
+            views = 0
+
+            if history.messages:
+                views = getattr(history.messages[0], "views", 0)
+
+            return {
+                "client": client,
+                "userbot_id": userbot_id,
+                "entity": entity,
+                "title": entity.title,
+                "subscribers": subscribers,
+                "views": views
+            }
+
+        except:
+            continue
+
+    return None
