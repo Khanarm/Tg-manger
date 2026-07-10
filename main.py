dfd34cd0c8c369452b1f02c4ebc85227ef31ba5b
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-10 17:32:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,22 +3,28 @@ import asyncio
 from aiogram import Bot, Dispatcher
 
 from config import BOT_TOKEN
-from handlers.start import router
-from userbot.client import start_userbots
+from handlers.start import router as start_router
 from handlers.setname import router as setname_router
+from userbot.client import start_userbots
 
 bot = Bot(BOT_TOKEN)
 
 dp = Dispatcher()
 
-dp.include_router(router)
+# Routers
+dp.include_router(start_router)
+dp.include_router(setname_router)
+
 
 async def main():
+    # Start all Telethon userbots
     await start_userbots()
 
-    print("Bot Started")
+    print("✅ Bot Started")
 
+    # Start Aiogram bot
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
