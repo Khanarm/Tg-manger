2ba610b5656bbd7dd41d3c2631557294265febe6
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-10 17:19:57 +0530

    Update and rename bot.py to main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+
+from config import BOT_TOKEN
+from handlers.start import router
+from userbot.client import start_userbots
+
+bot = Bot(BOT_TOKEN)
+
+dp = Dispatcher()
+
+dp.include_router(router)
+
+async def main():
+    await start_userbots()
+
+    print("Bot Started")
+
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
