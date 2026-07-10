9abeaa070538e539900b8d7fbd347beab39512f8
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-10 17:15:56 +0530

    Update client.py

--- userbot/client.py
+++ userbot/client.py
@@ -1,52 +1,22 @@
 from telethon import TelegramClient
 from telethon.sessions import StringSession
 
-from config import API_ID, API_HASH
-from database.mongo import get_all_userbots
+from config import API_ID, API_HASH, STRING_SESSIONS
 
-# Store all running clients
-clients = {}
-
+clients = []
 
 async def start_userbots():
-    """
-    Start all saved userbots from MongoDB.
-    """
-    bots = await get_all_userbots()
+    for session in STRING_SESSIONS:
+        client = TelegramClient(
+            StringSession(session),
+            API_ID,
+            API_HASH
+        )
 
-    for bot in bots:
-        try:
-            session = bot["session"]
-            user_id = bot["user_id"]
+        await client.start()
 
-            client = TelegramClient(
-                StringSession(session),
-                API_ID,
-                API_HASH
-            )
+        me = await client.get_me()
 
-            await client.start()
+        print(f"Started: {me.first_name}")
 
-            me = await client.get_me()
-
-            clients[user_id] = client
-
-            print(f"✅ UserBot Started : {me.first_name} ({user_id})")
-
-        except Exception as e:
-            print(f"❌ Failed to start UserBot {bot.get('user_id')}")
-            print(e)
-
-
-def get_client(user_id: int):
-    """
-    Return running client by userbot id.
-    """
-    return clients.get(user_id)
-
-
-def get_all_clients():
-    """
-    Return all running clients.
-    """
-    return clients
+        clients.append(client)
