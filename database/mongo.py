23fc145fca0e8786fb71e7a7264941ad3209573f
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-11 19:34:21 +0530

    Update mongo.py

--- database/mongo.py
+++ database/mongo.py
@@ -1,1 +1,14 @@
+from motor.motor_asyncio import AsyncIOMotorClient
 
+from config import (
+    MONGO_URI,
+    DATABASE_NAME,
+)
+
+client = AsyncIOMotorClient(MONGO_URI)
+
+db = client[DATABASE_NAME]
+
+channels = db.channels
+settings = db.settings
+broadcast_logs = db.broadcast_logs
