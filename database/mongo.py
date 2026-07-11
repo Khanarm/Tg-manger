c0966464741a9b259081aabbaaea5d2ad7a5f2ab
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-11 19:43:23 +0530

    Update mongo.py

--- database/mongo.py
+++ database/mongo.py
@@ -1,4 +1,5 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+from datetime import datetime
 
 from config import (
     MONGO_URI,
@@ -12,3 +13,40 @@ db = client[DATABASE_NAME]
 channels = db.channels
 settings = db.settings
 broadcast_logs = db.broadcast_logs
+
+
+async def save_channel(
+    channel_id: int,
+    title: str,
+    username: str = None,
+):
+    await channels.update_one(
+        {
+            "channel_id": channel_id
+        },
+        {
+            "$set": {
+                "title": title,
+                "username": username,
+                "updated_at": datetime.utcnow()
+            }
+        },
+        upsert=True
+    )
+
+
+async def get_channel(channel_id: int):
+    return await channels.find_one(
+        {
+            "channel_id": channel_id
+        }
+    )
+
+
+async def get_all_channels():
+    result = []
+
+    async for channel in channels.find():
+        result.append(channel)
+
+    return result
