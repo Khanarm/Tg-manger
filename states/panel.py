9e3144f1cd37c7823100a4ed52193bb5657e0baf
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-12 14:40:03 +0530

    Update panel.py

--- states/panel.py
+++ states/panel.py
@@ -2,14 +2,18 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class PanelState(StatesGroup):
+    # Normal features
     waiting_name = State()
     waiting_username = State()
     waiting_photo = State()
     waiting_bio = State()
 
-    # Automation
-    waiting_action = State()
-    waiting_value = State()
+    # Scheduled automation
+    waiting_schedule_name = State()
+    waiting_schedule_username = State()
+    waiting_schedule_photo = State()
+    waiting_schedule_post = State()
     waiting_date = State()
     waiting_time = State()
-    waiting_post = State()
+
+    waiting_confirm = State()
