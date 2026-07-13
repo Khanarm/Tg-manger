647e1932a82fad40600f267432a9df8ecb21eb6f
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-13 23:20:28 +0530

    Update task_runner.py

--- automation/task_runner.py
+++ automation/task_runner.py
@@ -23,7 +23,7 @@ async def execute_task(task):
 
         channel_id = task["channel_id"]
         data = task["data"]
-        action = data["update_channel"]
+        action = data["action"]
 
         if action == "rename":
 
