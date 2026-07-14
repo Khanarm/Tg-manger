aaddf87ea0078d127a63a7011ec00acd90955961
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-14 11:51:24 +0530

    Create loader.py

---
+++ loader.py
@@ -0,0 +1,10 @@
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+
+from config import BOT_TOKEN
+
+bot = Bot(BOT_TOKEN)
+
+storage = MemoryStorage()
+
+dp = Dispatcher(storage=storage)
